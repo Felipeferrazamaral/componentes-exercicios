cf93906b5fb49d4e1379d7816b45df7bee2876d1
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-05-11 21:29:37 -0300

    att trabalho

---
+++ src/componentes/CardVideo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import InfosUsuario from './InfosUsuario'
+function CardVideo() {
+
+    const titulo = "Título do vídeo";
+
+    function reproduzVideo () {
+        alert("O video esta sendo reproduzido")
+    }
+
+
+    
+  return (
+    <div className="box-pagina-principal" onClick={reproduzVideo}>
+        
+   
+    <InfosUsuario/>
+    
+  </div>
+  )
+}
+
+export default CardVideo
